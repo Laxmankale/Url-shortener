30a23007212c40eb329f1b045e9bfe9d5db869fa
Laxman Kale <lakhankale888@gmail.com>
2026-04-06 15:04:01 +0530

    add UrlRepository and initialize database with a sample URL

--- src/main/java/com/example/urlshortener/UrlshortenerApplication.java
+++ src/main/java/com/example/urlshortener/UrlshortenerApplication.java
@@ -1,7 +1,13 @@
 package com.example.urlshortener;
 
+import com.example.urlshortener.model.Url;
+import com.example.urlshortener.repository.UrlRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class UrlshortenerApplication {
@@ -9,5 +15,16 @@ public class UrlshortenerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UrlshortenerApplication.class, args);
 	}
+    @Bean
+    CommandLineRunner testDb(UrlRepository repo) {
+        return args -> {
+            Url url = new Url();
+            url.setLongUrl("https://google.com");
+            url.setCreatedAt(LocalDateTime.now());
 
+            repo.save(url);
+
+            System.out.println("Saved URL with ID: " + url.getId());
+        };
+    }
 }
